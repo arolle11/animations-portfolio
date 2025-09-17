cee4a6a82e0d0aeb5b2dc11d25828d78ee662059
Arolle11 <arollefona11@gmail.com>
2025-09-17 21:39:10 +0100

    updates on todo list animation

--- src/components/animations/TodoList.tsx
+++ src/components/animations/TodoList.tsx
@@ -1,5 +1,5 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { Check } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 
 const TodoList = () => {
