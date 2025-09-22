4bf38414f89897c2f2a77c89303f0ab93c8f231d
Arolle11 <arollefona11@gmail.com>
2025-09-22 20:58:20 +0100

    updates on checkbox interaction

--- src/components/animations/MessagingAppInput.tsx
+++ src/components/animations/MessagingAppInput.tsx
@@ -1,10 +1,6 @@
 const MessagingAppInput = () => {
   return (
-    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, sed ut,
-      dolore reiciendis soluta ratione eligendi doloribus hic sequi aliquid sunt
-      molestias esse nostrum, in nihil vitae neque eveniet eum.
-    </div>
+    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md"></div>
   );
 };
 export default MessagingAppInput;
