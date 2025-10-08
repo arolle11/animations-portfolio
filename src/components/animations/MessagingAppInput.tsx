a8a27294e1467185a8351bc46ab483817d54c947
Arolle11 <arollefona11@gmail.com>
2025-10-08 22:51:06 +0100

    messaging app input micro animation

--- src/components/animations/MessagingAppInput.tsx
+++ src/components/animations/MessagingAppInput.tsx
@@ -1,4 +1,12 @@
-import { Send, Plus } from "lucide-react";
+import {
+  Send,
+  Plus,
+  Camera,
+  Images,
+  FileText,
+  Volume2,
+  FolderOpen,
+} from "lucide-react";
 import { motion } from "motion/react";
 import { useState } from "react";
 
@@ -7,13 +15,11 @@ export default function MessagingAppInput() {
 
   return (
     <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-secondary gap-8 p-8 rounded-md">
-      <motion.div
-        className="flex items-center w-full border-2 border-[#c7cbd9] rounded-full px-4"
-        onMouseEnter={() => setIsTyping(true)}
-        onMouseLeave={() => setIsTyping(false)}
-      >
+      <motion.div className="flex items-center w-full border-2 border-[#c7cbd9] rounded-full px-4">
         <motion.span
           className="rounded-full border-2 border-gray-500 p-1 inline-block text-gray-500"
+          onMouseEnter={() => setIsTyping(true)}
+          onMouseLeave={() => setIsTyping(false)}
           initial={{
             rotate: 0,
             color: "#6b7280",
@@ -48,9 +54,25 @@ export default function MessagingAppInput() {
               opacity: isTyping ? 1 : 0,
             }}
             transition={{ duration: 0.4, ease: "easeInOut" }}
-            className="absolute left-4"
+            className="absolute left-4 w-full pr-16"
           >
-            loading...
+            <span className="flex justify-between w-full text-gray-500">
+              <span className="border rounded-md p-1.5 hover:bg-gray-200 transition-colors">
+                <Camera size={18} />
+              </span>
+              <span className="border rounded-md p-1.5 hover:bg-gray-200 transition-colors">
+                <Images size={18} />
+              </span>
+              <span className="border rounded-md p-1.5 hover:bg-gray-200 transition-colors">
+                <FileText size={18} />
+              </span>
+              <span className="border rounded-md p-1.5 hover:bg-gray-200 transition-colors">
+                <Volume2 size={18} />
+              </span>
+              <span className="border rounded-md p-1.5 hover:bg-gray-200 transition-colors">
+                <FolderOpen size={18} />
+              </span>
+            </span>
           </motion.span>
 
           <motion.span
