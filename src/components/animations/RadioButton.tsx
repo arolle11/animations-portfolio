b95c425944b2974f6cc7cbb99f1bd9c799f3ac0d
Arolle11 <arollefona11@gmail.com>
2025-10-09 22:24:52 +0100

    messaging app input micro animation

--- src/components/animations/RadioButton.tsx
+++ src/components/animations/RadioButton.tsx
@@ -1,5 +1,9 @@
 export default function RadioButton() {
   return (
-    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md"></div>
+    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolor maiores
+      cumque earum illo ratione voluptate iusto enim. Minima delectus aliquid
+      cum vero et unde possimus aut facere numquam beatae.
+    </div>
   );
 }
