c5aba27de0ef729586d3af3be11ff16ade9872b4
Arolle11 <arollefona11@gmail.com>
2025-10-11 21:47:29 +0100

    Radio button animation

--- src/components/animations/RadioButton.tsx
+++ src/components/animations/RadioButton.tsx
@@ -1,9 +1,22 @@
 export default function RadioButton() {
   return (
-    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolor maiores
-      cumque earum illo ratione voluptate iusto enim. Minima delectus aliquid
-      cum vero et unde possimus aut facere numquam beatae.
+    <div className="cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1]  gap-8 p-8 rounded-md">
+      <div className="flex flex-col bg-white text-secondary rounded-lg p-12 gap-8">
+        <span className="flex w-24 justify-between items-center">
+          {" "}
+          <label htmlFor="one">One</label>
+          <input type="radio" name="one" id="one" />
+        </span>
+        <span className="flex w-24 justify-between items-center">
+          {" "}
+          <label htmlFor="two">Two</label>
+          <input type="radio" name="one" id="two" />
+        </span>
+        <span className="flex w-24 justify-between items-center">
+          <label htmlFor="three">Three</label>
+          <input type="radio" name="one" id="three" />
+        </span>
+      </div>
     </div>
   );
 }
